537a736aeb445ab8db72ab51fdaec130b148e6f7
madymadhan12 <22hr1a3238@mtieat.org>
2024-09-19 15:01:40 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -6,4 +6,10 @@ Sample Input:
 Sample Output:
 10 20 30 40 50 
 ...
+solution
+numbers = [5, 2, 9, 1, 5, 6]
+sorted_numbers = sorted(numbers)
+print("Sorted list:", sorted_numbers)
+....
+o/p:Sorted list: [1, 2, 5, 5, 6, 9]
 
